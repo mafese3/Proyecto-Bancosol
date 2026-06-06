578c7fd4b5cb3f5c065ec07511f9d630cc6ab549
Danieh <148767868+daniiroblees@users.noreply.github.com>
2026-06-06 15:46:32 +0200

    Created usuario rest controller with his own methods

---
+++ src/main/java/com/leftjoiners/bancosol/proyectobackend/controller/rest/UsuarioRestController.java
@@ -0,0 +1,31 @@
+package com.leftjoiners.bancosol.proyectobackend.controller.rest;
+
+import com.leftjoiners.bancosol.proyectobackend.dto.Distrito;
+import com.leftjoiners.bancosol.proyectobackend.dto.Usuario;
+import com.leftjoiners.bancosol.proyectobackend.service.DistritoService;
+import com.leftjoiners.bancosol.proyectobackend.service.UsuarioService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@AllArgsConstructor
+@RequestMapping("/api/usuarios")
+public class UsuarioRestController {
+    private final UsuarioService usuarioService;
+
+    @GetMapping("/coordinadores")
+    public List<Usuario> getCoordinadores() {
+        return usuarioService.listarCoordinadores(); // Ajusta según tu lógica de filtrado
+    }
+
+    @GetMapping("/capitanes")
+    public List<Usuario> getCapitanes() {
+        return usuarioService.listarCapitanes();
+    }
+}
